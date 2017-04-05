7fbbc72e5adb55a15310858c3ed47789c3d5efd7
Gary Wong <gary.wonghg@gmail.com>
2017-04-05 16:12:22 -0700

    Create App component

---
+++ public/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }  
+
+  render () {
+
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
