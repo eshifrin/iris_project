a51413f5f8b331aba1b4b80b77ecbd1c2c0f50da
E Shifrin <eshifrin@gmail.com>
2017-04-16 19:50:53 -0700

    heroku 3

--- server/Auth/Auth0.js
+++ server/Auth/Auth0.js
@@ -2,7 +2,9 @@ const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 var dotenv = require('dotenv');
 dotenv.load();
-console.log('process.env', process.env.AUTH0_DOMAIN);
+console.log('process.env domain', process.env.AUTH0_DOMAIN);
+console.log('process.env callback', process.env.AUTH0_CALLBACK_URL);
+
 
 // This will configure Passport to use Auth0
 
--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ const rh = require('./routeHandler')
 const cronJob = require('./cronJob.js');
 
 // sm.getPostsStats();
-console.log('here is the callback url', process.env.AUTH0_CALLBACK_URL)
 
 // const user = require('./routes/usercred');
 const cloudinary = require('cloudinary');
