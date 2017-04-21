71e523503337458b2f6038fa623c779d915efcb3
Gary Wong <gary.wonghg@gmail.com>
2017-04-20 23:22:17 -0700

    Render Facebook likes and comments count

--- public/src/components/PastPostListItem.jsx
+++ public/src/components/PastPostListItem.jsx
@@ -21,7 +21,7 @@ const PastPostListItem = ({post, handleResubmitClick}) => {
   const monthDay = dateObj.format('M/D');
   const time = dateObj.format('hh:mma');
   // console.log('post id in item: ', post._id);
-  
+
   return (
     <Card style={{width: '30%'}}>
       <CardHeader
@@ -36,8 +36,10 @@ const PastPostListItem = ({post, handleResubmitClick}) => {
       {post.postToTwitter && <FlatButton secondary={!post.postedTwitterId}> Twitter</FlatButton>}
       {post.postToFacebook && <FlatButton secondary={!post.postedFacebookId}> Facebook</FlatButton>}
       <FlatButton primary={true} onClick={e => handleResubmitClick(e, post)} >Repost</FlatButton>
-      <FlatButton> Favorite Count: {post.twFavCount}</FlatButton>
-      <FlatButton> Retweet Count: {post.twRetweetCount}</FlatButton>
+      <FlatButton> TW Fav Count: {post.twFavCount}</FlatButton>
+      <FlatButton> TW Retweet Count: {post.twRetweetCount}</FlatButton>
+      <FlatButton> FB Likes Count: {post.fbLikeCount}</FlatButton>
+      <FlatButton> FB Comments Count: {post.fbCommentCount}</FlatButton>
     </Card>
   )
 };
