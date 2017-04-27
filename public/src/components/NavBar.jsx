4c28e6b9457508536d71d6275797ffd2cb57879f
Apurva Shastry <apurva.shastry@gmail.com>
2017-04-26 23:27:49 -0700

    added better logo, made drop down links work, fixed UI of the flat buttons in drop down links, conditional redering of the menu items in the drop down

--- public/src/components/NavBar.jsx
+++ public/src/components/NavBar.jsx
@@ -2,21 +2,29 @@ import React, { PropTypes } from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import { yellow800 } from 'material-ui/styles/colors';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 const propTypes = {
   login: PropTypes.bool.isRequired,
   twitter: PropTypes.bool.isRequired,
-  facebook: PropTypes.bool.isRequired
+  facebook: PropTypes.bool.isRequired,
 };
 
-const NavBar = ({login, twitter, facebook}) => {
-	var ButtonStyle = {
+const NavBar = ({ login, twitter, facebook }) => {
+  const ButtonStyleLive = {
     backgroundColor: 'transparent',
     color: yellow800,
   };
-  let loginOrLogout = login ? 'Login | Signup' : 'Log Out';
-  let loginLogoutHref = login ? '/login' : '/logout';
+  const ButtonStyleDead = {
+    backgroundColor: 'transparent',
+    color: 'grey',
+  };
+  const loginOrLogout = login ? 'Login | Signup' : 'Log Out';
+  const loginLogoutHref = login ? '/login' : '/logout';
   const Logged = (props) => (
     <IconMenu
       {...props}
@@ -26,28 +34,24 @@ const NavBar = ({login, twitter, facebook}) => {
       targetOrigin={{horizontal: 'right', vertical: 'top'}}
       anchorOrigin={{horizontal: 'right', vertical: 'top'}}
     >
-      <MenuItem primaryText="Verify Twitter" />
-      <MenuItem primaryText="Verify Facebook" />
-      <MenuItem primaryText="Logout" />
+      {twitter && <MenuItem rightIcon={<i className='fa fa-twitter fa-2x' style={ButtonStyleLive}/>} primaryText={<FlatButton href="/twitter" hoverColor={'#e6e6e6'}> Verify Twitter </FlatButton>} /> }
+      {!twitter && <MenuItem rightIcon={<i className='fa fa-twitter fa-2x' style={ButtonStyleDead}/>} primaryText={<FlatButton href="/deauthorize/twitter" hoverColor={'#e6e6e6'}> Unlink Twitter </FlatButton>} /> }
+      {facebook && <MenuItem rightIcon={<i className='fa fa-facebook fa-2x'style={ButtonStyleLive}/>} primaryText={<FlatButton href="/facebook" hoverColor={'#e6e6e6'}> Verify Facebook</FlatButton> } /> }
+      {!facebook && <MenuItem rightIcon={<i className='fa fa-facebook fa-2x'style={ButtonStyleDead}/>} primaryText={<FlatButton href="/deauthorize/facebook" hoverColor={'#e6e6e6'}> Unlink Facebook</FlatButton> } /> }
+      <MenuItem primaryText={<FlatButton href='/logout' label='Log Out' hoverColor={'#e6e6e6'}/>} />
     </IconMenu>
   );
 
-  const styles = { appBar: { position: "fixed",}, };
-	return (
-		<div>
-		<AppBar title={<img src="./img/logo.png" width={70} />}
-    style={styles.appBar}
-    showMenuIconButton={false} 
-    zDepth={1}
-    iconElementRight={<FlatButton href={loginLogoutHref} style={ButtonStyle} label={loginOrLogout}  />}
-    />
-      { !login &&  <i className='fa fa-twitter fa-2x' style={ButtonStyle}/> }
-      { !login && twitter && <FlatButton href="/twitter" style={ButtonStyle} label="verify Twitter"/>}
-      { !login && !twitter && <FlatButton href="/deauthorize/twitter" style={ButtonStyle} label="unlink Twitter"/>}
-      { !login && <i className='fa fa-facebook fa-2x'style={ButtonStyle}/> }
-      { !login && facebook && <FlatButton href="/facebook" style={ButtonStyle} label="verify Facebook"/>}
-      { !login && !facebook && <FlatButton href="/deauthorize/facebook" style={ButtonStyle} label="unlink Facebook" />}
-
+  return (
+    <div>
+      <AppBar title={<img src="./img/logo3.png" width={60}/>}
+        showMenuIconButton={false}
+        zDepth={1}
+        iconElementRight={login ? <FlatButton href={loginLogoutHref} style={ButtonStyleLive} label={loginOrLogout} /> : <div>
+          <FlatButton href={loginLogoutHref} style={ButtonStyleDead} label={loginOrLogout} />
+          <Logged />
+          </div>}
+      />
 	  </div>
   );
 };
