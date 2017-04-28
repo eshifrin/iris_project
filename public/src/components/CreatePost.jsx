856cce5ec90d0733860d767c5394b35d3a0b0f8e
Gary Wong <gary.wonghg@gmail.com>
2017-04-27 21:39:52 -0700

    Add new loader for create post

--- public/src/components/CreatePost.jsx
+++ public/src/components/CreatePost.jsx
@@ -2,11 +2,12 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import PhotoUpload from './PhotoUpload.jsx';
 import WriteMessage from './WriteMessage.jsx';
+import Loader from './Loader.jsx';
 import FlatButton from 'material-ui/FlatButton';
 import 'react-widgets/lib/less/react-widgets.less';
 import DTP from 'react-widgets/lib/DateTimePicker';
 import moment from 'moment';
-import momentLocalizer from 'react-widgets/lib/localizers/moment'
+import momentLocalizer from 'react-widgets/lib/localizers/moment';
 momentLocalizer(moment);
 
 import { handleScheduleChange,
@@ -17,73 +18,74 @@ import { handleScheduleChange,
         handlePostSubmit } from '../actions/permissions';
 
 class CreatePost extends React.Component {
-    render() {
-
+  render() {
     return (
-      <div style={{'textAlign': 'center', maxHeight: 'none ! important'}}>
-      <div style={{'fontWeight': 'bold', 'color': '#F9A825', fontSize: '200%'}}>share</div>
-      <div style={{width: '75%', textAlign: 'center', display: 'inline-block', marginTop: '5px'}}>
-        <DTP defaultValue={this.props.scheduledDateTime ? new Date(this.props.scheduledDateTime) : new Date()}
-             min={new Date()}
-             onChange={this.props.handleScheduleChange}
-        />
-      </div>
+      <div style={{ textAlign: 'center', maxHeight: 'none ! important' }}>
+        <div style={{ fontWeight: 'bold', color: '#F9A825', fontSize: '200%' }}>
+          share
+          {this.props.isLoading ? <Loader /> : null}
+        </div>
+        <div style={{ width: '75%', textAlign: 'center', display: 'inline-block', marginTop: '5px' }}>
+          <DTP
+            defaultValue={this.props.scheduledDateTime ? new Date(this.props.scheduledDateTime) : new Date()}
+            min={new Date()}
+            onChange={this.props.handleScheduleChange}
+          />
+        </div>
 
-        <WriteMessage/>
+        <WriteMessage />
 
-        <div style={{height: '175'}}>
-        <PhotoUpload
-          uploadImg={this.props.uploadImg}
-          imgUrl={this.props.imgUrl}
-          handleClearImg={this.props.handleClearImg}
-        />
+        <div style={{ height: '175' }}>
+          <PhotoUpload
+            uploadImg={this.props.uploadImg}
+            imgUrl={this.props.imgUrl}
+            handleClearImg={this.props.handleClearImg}
+          />
         </div>
 
 
-      <div>
-      <hr />
+        <div>
+          <hr />
 
-      <div style={{textAlign: 'center', display: 'inline-block'}} >
-        { (this.props.postToFacebook || this.props.postToTwitter) &&  <FlatButton primary={true} value="postnow" onClick={this.props.handleNowSubmit}>post now</FlatButton> }
-        { (this.props.postToFacebook || this.props.postToTwitter) &&<FlatButton primary={true} value="schedulepost" onClick={this.props.handlePostSubmit}>schedule post</FlatButton> }
-        {!this.props.postToTwitter && !this.props.postToFacebook && <h4 style={{color: 'red'}}>please choose at least one social site to share your post</h4>}
+          <div style={{ textAlign: 'center', display: 'inline-block' }} >
+            { (this.props.postToFacebook || this.props.postToTwitter) && <FlatButton primary value="postnow" onClick={this.props.handleNowSubmit}>post now</FlatButton> }
+            { (this.props.postToFacebook || this.props.postToTwitter) && <FlatButton primary value="schedulepost" onClick={this.props.handlePostSubmit}>schedule post</FlatButton> }
+            {!this.props.postToTwitter && !this.props.postToFacebook && <h4 style={{ color: 'red' }}>please choose at least one social site to share your post</h4>}
+          </div>
+        </div>
+
       </div>
-      </div>
+    );
+  }
+}
 
-    </div>
-  )}
-};
+const mapStateToProps = state => ({
+  postToTwitter: state.main.postToTwitter,
+  postToFacebook: state.main.postToFacebook,
+  imgUrl: state.main.imgUrl,
+  scheduledDateTime: state.main.scheduledDateTime,
+  isLoading: state.main.isLoading,
+});
 
-const mapStateToProps = (state) => {
-  return {
-    postToTwitter: state.main.postToTwitter,
-    postToFacebook: state.main.postToFacebook,
-    imgUrl: state.main.imgUrl,
-    scheduledDateTime: state.main.scheduledDateTime,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    handleScheduleChange: (e) => {
-      dispatch(handleScheduleChange(e));
-    },
-    uploadImg: (e) => {
-      dispatch(uploadImg(e));
-    },
-    handleClearImg: () => {
-      dispatch(handleClearImg());
-    },
-    handleResetPostFields: () => {
-      dispatch(handleResetPostFields());
-    },
-    handleNowSubmit: () => {
-      dispatch(handleNowSubmit('now'));
-    },
-    handlePostSubmit: () => {
-      dispatch(handlePostSubmit());
-    }
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  handleScheduleChange: (e) => {
+    dispatch(handleScheduleChange(e));
+  },
+  uploadImg: (e) => {
+    dispatch(uploadImg(e));
+  },
+  handleClearImg: () => {
+    dispatch(handleClearImg());
+  },
+  handleResetPostFields: () => {
+    dispatch(handleResetPostFields());
+  },
+  handleNowSubmit: () => {
+    dispatch(handleNowSubmit('now'));
+  },
+  handlePostSubmit: () => {
+    dispatch(handlePostSubmit());
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreatePost);
--- public/src/components/Loader.jsx
+++ public/src/components/Loader.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import loader from '../../../public/dist/img/loader.gif';
+import loader from '../../../public/dist/img/spin.gif';
 
 const Loader = (props) => (
   <div>
-    <img src={loader} />
+    <img src={loader} style={{ width: '7%', display: 'inline-block' }} />
   </div>
   );
 
